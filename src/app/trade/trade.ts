2e700716ccfcd79cc82670552a31b015975f1115
DotNet14 <naizuddin12@gmail.com>
2025-12-10 19:24:39 +0530

    Certificate 4

---
+++ src/app/trade/trade.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trade',
+  imports: [],
+  templateUrl: './trade.html',
+  styleUrl: './trade.css',
+})
+export class Trade {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Trade } from './app/trade/trade';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(Trade, appConfig)
   .catch((err) => console.error(err));
