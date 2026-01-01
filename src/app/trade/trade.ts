36e297c8a022f43f2e7a851c1904853e2e10a283
DotNet14 <naizuddin12@gmail.com>
2026-01-02 01:17:34 +0530

    Updated File

--- src/app/trade/trade.ts
+++ src/app/trade/trade.ts
@@ -7,32 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './trade.css',
 })
 export class Trade {
-_Var_ServiceNameEN_Var_:string='';
-_Var_ServiceNameAR_Var_:string='';
-_Var_RequestCode_Var_:string='';
-_Var_TradeNameEN_Var_:string='';
-_Var_TradeNameAR_Var_:string='';
-_Var_LegalFormEN_Var_:string='';
-_Var_LegalFormAR_Var_:string='';
-_Var_ActivityEN_Var_:string='';
-_Var_ActivityAR_Var_:string='';
-_Var_PeriodEN_Var_:string='';
-_Var_PeriodAR_Var_:string='';
-_Var_ReservationStartDate_Var_:string='';
-_Var_ReservationEndDate_Var_:string='';
-_Var_ApplicantNameEN_Var_:string='';
-_Var_ApplicantNameAR_Var_:string='';
-_Var_IDOrPassportNumber_Var_:string='';
-_Var_Email_Var_:string='';
-_Var_Phone_Var_:string='';
-_Var_NationalityEN_Var_:string='';
-_Var_NationalityAR_Var_:string='';
-_Var_ApplicantAccountTypeEN_Var_:string='';
-_Var_ApplicantAccountTypeAR_Var_:string='';
-_Var_PrintDate_Var_:string='';
-_Var_GeneratedBy_Var_:string='';
-_Var_PaymentVoucher_Var_:string='';
-_Var_PaymentTransaction_Var_:string='';
-_Var_QRCode_Var_:string='';
 
 }
